0b9456a103dc22e2d00ef1d6b435b458e0052230
hyuna <hyuna_0525@dsm.hs.kr>
2023-07-28 21:10:29 +0900

    ADD : 페이지 연결

---
+++ src/components/pages/index.jsx
@@ -0,0 +1,13 @@
+import Head from "../common/index.jsx";
+import MainPage from "../common/recommend.jsx";
+
+function RecommendPage() {
+  return (
+    <div>
+      <Head />
+      <MainPage />
+    </div>
+  );
+}
+
+export default RecommendPage;

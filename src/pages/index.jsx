89a5ebb0d611f673ef030fab59e6063359479731
hyuna <hyuna_0525@dsm.hs.kr>
2023-07-30 11:08:34 +0900

    CHORE : 문서 이동

---
+++ src/pages/index.jsx
@@ -0,0 +1,13 @@
+import Head from "../components/common/index";
+import MainPage from "../components/pages/index";
+
+function RecommendPage() {
+  return (
+    <div>
+      <Head />
+      <MainPage />
+    </div>
+  );
+}
+
+export default RecommendPage;
